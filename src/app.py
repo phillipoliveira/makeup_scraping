2733e4c1c890e16380dcd5b40778008710a309f9
Phillip Oliveira <phillip.j.oliveira@gmail.com>
2018-12-09 23:19:05 -0500

    added timing

--- src/app.py
+++ src/app.py
@@ -1,12 +1,16 @@
 from targets.sephora import Sephora
 from pprint import pprint
+import time
 
 
 def scrape_sephora():
+    t1 = time.time()
     sephora = Sephora()
     subcategories = sephora.get_all_subcategory_links()
     pprint(subcategories)
     for subcategory in subcategories:
         sephora.get_products(subcategory)
+    t2 = time.time()
+    print("This took  {} seconds".format(t2-t1))
 
 scrape_sephora()
